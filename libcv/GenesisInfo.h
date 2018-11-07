fde7a12be1fa7755dd8b1dc9a9a89cd6fa1235af
Cybervein <38497733+Cyberveinofficial@users.noreply.github.com>
2018-11-07 19:49:36 +0800

    Create GenesisInfo.h

---
+++ libcv/GenesisInfo.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include <string>
+#include <libdevcrypto/Common.h>
+
+namespace dev
+{
+namespace cv
+{
+
+extern std::string const c_genesisInfoTestBasicAuthority;
+extern dev::Addresses childDaos();
+
+}
+}
